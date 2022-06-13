e30dae59066b428450c959a53233b627514f042f
fakhri <blitzfakhri@gmail.com>
2022-06-13 08:22:32 +0700

    update: style

--- src/components/ListsUser.jsx
+++ src/components/ListsUser.jsx
@@ -5,8 +5,8 @@ import UserContext from "../UserContext";
 const UserContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
-  column-gap: 0.5em;
+  justify-content: center;
+  column-gap: 1.5em;
   @media (max-width: 768px) {
     flex-direction: column;
     align-items:center;
@@ -15,26 +15,29 @@ const UserContainer = styled.div`
 const UserRow = styled.div`
   display:flex;
   align-items: center;
-  justify-content:flex-start;
+  justify-content: space-between;
   font-size:1rem;
-  padding:0.5em;
+  padding:0.5em 1.5em;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   border-radius:20px;
   margin-bottom:1em;
-  min-width: 20vw;
+  width: min(10vw,100%);
   column-gap: 1.5em;
+  @media (max-width: 1440px) {
+    width: min(20vw,100%);
+  }
   @media (max-width: 925px) {
-    width: 30vw;
+    width: min(30vw,100%);
   }
   @media (max-width: 768px) {
-    width:50vw;
+    width: min(50vw,100%);
     
   }
   @media (max-width:480px){
-    width:70vw;
+    width: min(70vw,100%);
   }
   @media (max-width:320px){
-    width:80vw;
+    width: min(80vw,100%);
   }
 
 
